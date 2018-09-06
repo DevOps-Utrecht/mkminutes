bb57af9277b3a9d00d95c833f5b070fb7596d5d9
Robin Sikkens <github@robinsikkens.nl>
2018-09-06 15:33:48 +0200

    removed config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-MEMBERS = ["Hugo Peters", "Robin Sikkens", "Emiel van Rijn", "Glenn Stewart", "Robert Groot", "Maarten van den Berg", "Rogier Simons", "Roman Peters", "Yorick van Zweeden"]
-SECRETARY = "Robin Sikkens"
-CHAIRMAN = "Roman Peters"
